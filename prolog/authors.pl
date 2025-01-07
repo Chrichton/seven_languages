544ff753a7a97036aef0261c21b454614cbe1393
Heiko Goes <heiko.goes@gaebler-goes.de>
2025-01-07 10:12:49 +0100

    Prolog Day1

---
+++ prolog/authors.pl
@@ -0,0 +1,6 @@
+book_of(jurassic_park, michael_crichton).
+book_of(terminal_man, michael_crichton).
+book_of(narziss_und_goldmund, hermann_hesse).
+book_of(steppenwolf, hermann_hesse).
+
+book_of(Book, hermann_hesse).
---
+++ prolog/musicians.pl
@@ -0,0 +1,15 @@
+plays(igor_levit, piano).
+plays(beethoven, piano).
+plays(mick_jagger, guitar).
+plays(ringo_starr, drums).
+plays(paul_mc_cartney, guitar).
+plays(george_harrison, guitar).
+
+genre(igor_levit, classic).
+genre(beethoven, classic).
+genre(mick_jagger, rock).
+genre(ringo_starr, rock).
+genre(paul_mc_cartney, rock).
+genre(george_harrison, rock).
+
+plays(Who, guitar).
